dc8351512a591f2ccfc7ca02582f6b6f842b184f
nm0i <nm0i@me0w.net>
2020-05-20 21:37:40 +0300

    Balance fix.

--- R/paypal.R
+++ R/paypal.R
@@ -3,5 +3,6 @@
 read.paypal <- function(...) {
     tmptable <- read.csv(...);
     tmptable$Amount <- gsub(",", "", tmptable$Amount);
+    tmptable$Balance <- gsub(",", "", tmptable$Balance);
     tmptable;
 }
